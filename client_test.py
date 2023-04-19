08cc7d4efc1ffa60421f57e1b54c4bd6e30a6121
Timmy-Edibo <timothyedibo@gmail.com>
2023-04-19 09:18:03 +0100

    added test assertions

--- client_test.py
+++ client_test.py
@@ -8,13 +8,35 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    
+    expected = [
+      ('ABC', 120.48, 121.2, 120.84),
+      ('DEF', 117.87, 121.68, 119.775)
+    ]
+    
+    for i in range(len(quotes)):
+      assert getDataPoint(quotes[i])[-1] ==expected[i][-1]
 
+      
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    
+    expected = [
+      ('ABC', 120.48, 119.2, 119.84),
+      ('DEF', 117.87, 121.68, 119.775)
+    ]
+    
+    
+    for i in range(len(quotes)):
+      res = getDataPoint(quotes[i])
+      exp = expected[i]
+      assert ( res[1] > res[2] ) == (exp[1] > exp[2] )
+
+
 
 
   """ ------------ Add more unit tests ------------ """
